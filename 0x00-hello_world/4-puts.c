5fb1e8bbd48216817743939b04e5e2072789929c
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-02-16 09:21:06 -0800

    Updated task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,14 @@
 
 
 /**
- *Description:	This file print out a statement using the puts function 
- *main:	Entry point
- *Return:	Always 0 (success)
+ * main - This file print out a statement using the puts function
+ *
+ * Return: 0 if success
  */
 
 
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	
 }
