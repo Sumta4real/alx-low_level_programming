380051d7b2acdec23710998fe5a5c94139ae29a0
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-02-16 09:26:45 -0800

    Added task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+
+/**
+ * main - print a statement using the printf function
+ *
+ * Return: 0 if successful
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
