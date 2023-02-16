4207d5d73c34297efc7c3a6b36c7e88fd863db80
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-02-16 08:18:08 -0800

    Added task 4 and advanced task 100

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+
+/* this program prints a statement usinng the puts function */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+  
+}
