ae5f4f7a7e49541c187af3a0028a81b51f234fc6
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-07 12:12:09 -0800

    modified task0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
  */
 
 void print_alphabet(void)
-{       
-        char ch = 'a';
-        for (ch='a'; ch <='z'; ch++)
-        {
-               _putchar(ch);
-        }
-        _putchar('\n');
-}       
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
