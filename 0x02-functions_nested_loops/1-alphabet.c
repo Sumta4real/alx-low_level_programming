4f80287a496e5347b3def1c0694fa8c0884aa130
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-07 09:30:34 -0800

    Added task1 result

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This file prints all alphabets in lower case
+ * Return: 0 if success
+ */
+
+int main()
+{       
+        char ch = 'a';
+        for (ch='a'; ch <='z'; ch++)
+        {
+                _putchar(ch);
+        }
+        _putchar('\n');
+}       
