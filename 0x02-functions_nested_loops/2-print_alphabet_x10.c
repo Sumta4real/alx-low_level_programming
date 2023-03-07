fee99cdac6ef3c1868a720490f94e5461ffed2ba
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-07 12:58:23 -0800

    Added task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print lowercase alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int increase = 0;
+
+	while increase <= 9
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			 _putchar(ch);
+		}               
+
+		_putchar('\n');
+		 increase++
+	}
+}
