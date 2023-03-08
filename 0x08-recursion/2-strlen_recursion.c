5608b935d7591390bf439d44f459399513a7ca15
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-08 15:01:14 -0800

    added task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: inputed string
+ *
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	_strlen_recursion(s);
+	s++;
+}
