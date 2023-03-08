78880f5bb75e47ef0b3874fca59138cb40878bc1
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-08 13:53:18 -0800

    modifies task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * void _puts_recursion - 
+ * void _puts_recursion - prints a string, followed by a new line
  */
 
 void _puts_recursion(char *s)
 {	
 	if (*s == '\0')
 	{	
-		_putchar('\n')
+		_putchar('\n');
 	}
 	_putchar(*s);
 	s++;
-	_puts_recusrion(s);
+	_puts_recursion(s);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -9,4 +10,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
