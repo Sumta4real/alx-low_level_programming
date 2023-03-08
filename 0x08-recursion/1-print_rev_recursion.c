cec687a3bf516c7b38bf453b0db59e1a85420eaa
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-08 14:23:50 -0800

    modified task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** * __print_rev_recursion - prints a string in the reverse  
+/** _print_rev_recursion - prints a string in the reverse
  * @s: the string to be printed
  */
 
@@ -8,7 +8,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		 _print_rev_recursion(s+1);
+		 _print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
