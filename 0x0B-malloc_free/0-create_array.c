9100070fbbabb6c090214755b9139dc1f7dc5df1
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-14 10:57:39 -0700

    Added task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - create array of characters
+ *
+ * Return:NULL (if fails), pointer to array (if successful)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+        char *arr;
+	unsigned int i;
+
+	arr = malloc(size * sizeof(char));
+	if (size == 0 || arr == NULL)
+	{	
+		return (NULL);
+	}
+        for (int i = 0; i < size; i++)
+        {
+                arr[i] = c;
+        }
+        return (arr);
+}
