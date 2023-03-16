32ca463f687b8f4821ae0f85a7b22d0d7f3b61ad
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-16 11:20:10 -0700

    modified task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,12 +8,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-        void *memory;
+	void *memory;
 
-        memory = malloc(b);
-        if (memory == NULL)
-        {
-                exit(98);
-        }
-        return (memory);
+	memory = malloc(b);
+	if (memory == NULL)
+	{
+		exit(98);
+	}
+	return (memory);
 }
