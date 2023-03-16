d9ad8f8106fc5ee8bd9706ad1b518408972bea46
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-16 03:14:02 -0700

    Modified task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -38,9 +38,9 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 
-	for (i = 0; i < height; i++;)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width; j++;)
+		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
 		}
