5bbcfec54b0281b6b0a7dabb9a02926cf1a3acbd
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-26 03:25:46 -0700

    Added task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new structure define dog basic info
+ *
+ * @name: First element of struct
+ * @age: second element of struct
+ * @owner: third element of struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
