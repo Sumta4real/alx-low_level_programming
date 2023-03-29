36db3405801f7c2d3eb5b681729b1f2f06542a89
Sumayyah Taiwo <sumty4deen@gmail.com>
2023-03-29 08:55:14 -0700

    Modified task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,8 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-
+	if (d->age == NULL)
+		d->age = 0;
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 
